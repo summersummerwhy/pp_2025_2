fcde016acd4ea25bd9cf8bb852f8d5739c312bd9
Seojin Min <summermin@snu.ac.kr>
2025-12-12 20:11:06 +0900

    re-solved pp-ass-4A

--- Assignment/pp-assignment-4A/src/main/scala/Main.scala
+++ Assignment/pp-assignment-4A/src/main/scala/Main.scala
@@ -32,49 +32,51 @@ import scala.util.control.TailCalls._
   * Timeout: 30 sec.
   */
 
-object Lazylist{
-  import Data.* 
-/**
-  * Problem 1-1
-  * 
-  * Implement instances of Iter and ListIF for Lazylist.
-  */
+object Lazylist {
+
+  import Data.*
+
+  /**
+   * Problem 1-1
+   *
+   * Implement instances of Iter and ListIF for Lazylist.
+   */
   sealed abstract class Lazylist[A] {
-    def get : Option[(A, Lazylist[A])]
+    def get: Option[(A, Lazylist[A])]
   }
+
   class LNil[A]() extends Lazylist[A] {
     def get = None
   }
+
   class LCons[A](hd: A, _tl: => Lazylist[A]) extends Lazylist[A] {
     private lazy val tl = _tl
-    def get = Some(hd,tl)
+    def get = Some(hd, tl)
   }
 
-  // List[A] <: Iter[A] 완료
-  // lazylist[A] <: Iter[A]을 해야함 -> Data 참고하면 될듯
-  implicit def lazylistIter[A]: Iter[Lazylist,A] = new {
-    def get[A] (l: Lazylist[A]) =
+
+  implicit def lazylistIter[A]: Iter[Lazylist, A] = new {
+    def get[A](l: Lazylist[A]) =
       l.get match {
         case None => None
-        case Some((hd, tl)) => Some(hd, tl)
+        case Some(hd, tl) => Some((hd, tl))
       }
   }
 
-  // 이제 lazylistListIF[A] <: ListIF[A]
-  // 아닌 것 같고 일단 구현해보겠음....
-  implicit def lazylistListIF[A]: ListIF[Lazylist,A] = new {
-    def empty[A] = LNil[A]()
-    def cons[A] (a: A, l: =>Lazylist[A]) = LCons[A](a, l)
+
+  implicit def lazylistListIF[A]: ListIF[Lazylist, A] = new {
+    def empty[A] = new LNil[A]()
+    def cons[A](a: A, l: => Lazylist[A]) = new LCons[A](a, l)
     def iter[A] = lazylistIter[A]
-    def head[A] (l: Lazylist[A]) =
+    def head[A](l: Lazylist[A]) =
       l.get match {
         case None => None
-        case Some(hd, tl) => Some(hd)
+        case Some((hd, _)) => Some(hd)
       }
-    def tail[A] (l: Lazylist[A]) =
+    def tail[A](l: Lazylist[A]) =
       l.get match {
-        case None => LNil[A]()
-        case Some(hd, tl) => tl
+        case None => new LNil[A]()
+        case Some((hd, tl)) => tl
       }
   }
 }
@@ -101,66 +103,53 @@ object Assignment4A:
   * 
   * remark : fibo[List] will cause error. 
   */
-  def fibo[L[_]] (implicit listIF : ListIF[L,Long]) : L[Long] = {
-    // cons(0, fibs(1, 1)) 호출
-    // LCons(0, fibs(1,1)) 호출
-    // 하지만 여기의 LCons는 =>, 그리고 안의 get에서 lazy val로 호출
-    // 이 시점에도 fibs(1,1)은 그대로 전달됐다가
-    // 누가 여기서 get을 부를때 fibs(1, 1) 평가 들어감
-    // 다음꼬리에서도 반복
-    def fibs(a: Long, b: Long): L[Long] =
-      listIF.cons(a, fibs(b, a + b))
+  def fibo[L[_]] (implicit listIF : ListIF[L,Long]) : L[Long] =
+    def makeFibo(a: Long, b: Long): L[Long] =
+      listIF.cons(a + b, makeFibo(b, a+b))
+    listIF.cons(0, listIF.cons(1, makeFibo(0, 1)))
 
-    fibs(0, 1)
-  }
-
+  def reverse[A, L[_]](l: L[A]) (implicit listIF : ListIF[L,A]) : L[A] =
+    @tailrec def reverseIn(lIn: L[A], result: L[A]): L[A] =
+      listIF.iter.get(lIn) match {
+        case None => result
+        case Some((hd, tl)) =>
+          reverseIn(tl, listIF.cons(hd, result))
+      }
+    reverseIn(l, listIF.empty)
 /** 
   * getN(l,n) is a list of first n elements of l.
   * 
   * ex): getN([1,2,3,4...], 3) = [1,2,3]
   */
-
-  @tailrec def reverse[A, L[_]](l: L[A], result: L[A]) (implicit listIF : ListIF[L,A]): L[A] = {
-    listIF.head(l) match {
-      case Some(a) => reverse(listIF.tail(l), listIF.cons(a, result))
-      case _ => result
-    }
-  }
-
-  def getN[A, L[_]](l: L[A], n: Int) (implicit listIF : ListIF[L,A]) : L[A] = {
-
-    @tailrec def getNInside(l2: L[A], m: Int, result: L[A]): L[A] = {
-      if (m == n) result
-      else
-        listIF.head(l2) match {
-          case Some(a) => getNInside(listIF.tail(l2), m + 1, listIF.cons(a, result))
-          case _ => result
+  def getN[A, L[_]](l: L[A], n: Int) (implicit listIF : ListIF[L,A]) : L[A] =
+    def getNInside(l2: L[A], count: Int, result: L[A]): L[A] =
+      if (count == n) result
+      else {
+        listIF.iter.get(l2) match {
+          case None => result
+          case Some((hd, tl)) =>
+            getNInside(tl, count + 1, listIF.cons(hd, result))
         }
-    }
+      }
+    reverse(getNInside(l, 0, listIF.empty))
 
-    // l, n을 parameter로 사용, listIF의 함수를 활용해서 구현
-    if (n == 0) listIF.empty
-    else
-      // head를 n번빼내서 cons로 합쳐서 돌려준다
-      val temp = getNInside(l, 0, listIF.empty)
-      reverse(temp, listIF.empty)
+  def cons[A, L[_]](x: A, l: L[A]) (implicit listIF : ListIF[L,A]) : L[A] =
+    listIF.cons(x, l)
 
-  }
+  def empty[A, L[_]](implicit listIF: ListIF[L, A]): L[A] =
+    listIF.empty
+
 
 /** 
   * toIList(l) is a List version of given list l.
   */
-  def toList[A, L[_]](x: L[A]) (implicit listIF : ListIF[L,A]) : List[A] = {
-    // L[A]를 List[A]로 바꿔서?
-    // 그러니까 더 좁은 버전으로.... 흠흠
-    val temp = List[A]()
-    val temp2 = List[A]()
-    @tailrec def appendToList(l: L[A], result: List[A]): List[A] = {
-      listIF.head(l) match {
-        case Some(a) => appendToList(listIF.tail(l), a::result)
-        case _ => result
+  def toList[A, L[_]](x: L[A]) (implicit listIF : ListIF[L,A]) : List[A] =
+    def toListInside(l2: L[A], result: List[A]): List[A] =
+      listIF.iter.get(l2) match {
+        case None => result
+        case Some ((hd, tl)) =>
+          toListInside(tl, cons(hd, result))
       }
-    }
+    reverse(toListInside(x, empty[A, List]))
 
-    reverse(appendToList(x, temp), temp2)
-  }
+
